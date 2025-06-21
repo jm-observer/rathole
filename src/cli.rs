751feaa23c7a98650392307399b2e5fc41c66d37
fengqi <362254883@qq.com>
2025-06-21 21:52:19 +0800

    remove build

--- src/cli.rs
+++ src/cli.rs
@@ -9,7 +9,7 @@ pub enum KeypairType {
 
 lazy_static! {
     static ref VERSION: &'static str =
-        option_env!("VERGEN_GIT_SEMVER_LIGHTWEIGHT").unwrap_or(env!("VERGEN_BUILD_SEMVER"));
+        "todo";
     static ref LONG_VERSION: String = format!(
         "
 Build Timestamp:     {}
@@ -21,14 +21,14 @@ cargo Target Triple: {}
 cargo Profile:       {}
 cargo Features:      {}
 ",
-        env!("VERGEN_BUILD_TIMESTAMP"),
-        env!("VERGEN_BUILD_SEMVER"),
-        option_env!("VERGEN_GIT_SHA"),
-        option_env!("VERGEN_GIT_COMMIT_TIMESTAMP"),
-        option_env!("VERGEN_GIT_BRANCH"),
-        env!("VERGEN_CARGO_TARGET_TRIPLE"),
-        env!("VERGEN_CARGO_PROFILE"),
-        env!("VERGEN_CARGO_FEATURES")
+        "todo",
+        "todo",
+        "todo",
+        "todo",
+        "todo",
+        "todo",
+        "todo",
+        "todo"
     );
 }
 
